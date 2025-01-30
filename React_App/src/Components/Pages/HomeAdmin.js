0b8fdf84c068008cdd7a32013364cc2d467c0554
Suryansh Gupta <suryansh804.sg@gmail.com>
2025-01-30 22:39:21 +0530

    added admin dashboard page

---
+++ React_App/src/Components/Pages/HomeAdmin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderAdmin from "./HeaderAdmin";
+
+const HomeAdmin = () => {
+  return (
+    <>
+      <HeaderAdmin />
+    </>
+  );
+};
+
+export default HomeAdmin;
