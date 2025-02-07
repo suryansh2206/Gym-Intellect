33ee829a1b3ae324fdcc8793501b2cabc1b9706a
Suryansh Gupta <suryansh804.sg@gmail.com>
2025-02-07 17:28:43 +0530

    minor changes

---
+++ SpringBoot_Backend/src/main/java/com/knowit/gymintellect/gym_intellect/entity/Membership.java
@@ -0,0 +1,87 @@
+package com.knowit.gymintellect.gym_intellect.entity;
+
+
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "membershipplan")
+public class Membership {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "membership_id")
+    private int membershipId;
+
+    @Column(name = "membership_plan", nullable = false, length = 100)
+    private String membershipPlan;
+
+    @Column(name = "start_date", nullable = false)
+    private Date startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private Date endDate;
+
+    @Column(name = "fees", nullable = false, precision = 10, scale = 2)
+    private BigDecimal fees;
+
+    @Column(name = "status", nullable = false, length = 20)
+    private String status;
+
+    // Getters and Setters
+    public int getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(int membershipId) {
+        this.membershipId = membershipId;
+    }
+
+    public String getMembershipPlan() {
+        return membershipPlan;
+    }
+
+    public void setMembershipPlan(String membershipPlan) {
+        this.membershipPlan = membershipPlan;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public BigDecimal getFees() {
+        return fees;
+    }
+
+    public void setFees(BigDecimal fees) {
+        this.fees = fees;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
